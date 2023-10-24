61e862e20ffad28aa1b78ced5e82d4bc3df08fb6
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-10-24 13:45:57 +0900

    Initial commit

---
+++ firstapp/src/main/java/com/dw/firstapp/HelloPageController.java
@@ -0,0 +1,13 @@
+package com.dw.firstapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloPageController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "redirect:/hello.html";
+	}
+}
---
+++ firstapp/src/main/java/com/dw/firstapp/MyServlet.java
@@ -0,0 +1,27 @@
+package com.dw.firstapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class MyServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
+			throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("<html><body>");
+		out.println("<h1>Hello, this is a simple servlet!</h1>");
+		out.println("</body></html>");
+	}
+}
+
+
+
+
+
