710c3e1efa2c53f1514b382444c019dfbf6fa3a5
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-10-24 17:32:27 +0900

    getBean

--- firstapp/src/main/java/com/dw/firstapp/FirstappApplication.java
+++ firstapp/src/main/java/com/dw/firstapp/FirstappApplication.java
@@ -3,13 +3,18 @@ package com.dw.firstapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FirstappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstappApplication.class, args);
+		var context = SpringApplication.run(FirstappApplication.class, args);
+		
+//		var context = 
+//				new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		System.out.println(context.getBean("age"));
 	}
 	
 	@Bean
