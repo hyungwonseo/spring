3d7a0b97de5993a5375393f8c028d52e2270a93a
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-10-25 16:48:20 +0900

    datasource

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e9/c070fb590773001e135e83905aa61c01
@@ -0,0 +1,9 @@
+package com.dw.employeeManagement;
+
+public class EmployeeManagement {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- firstapp/src/main/java/com/dw/employeeManagement/controller/EmployeeController.java
+++ firstapp/src/main/java/com/dw/employeeManagement/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.dw.employeeManagement.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
 
 }
