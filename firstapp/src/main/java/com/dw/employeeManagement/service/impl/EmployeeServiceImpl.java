053f5a942145e53803434107842dea9f4fff10df
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-10-26 12:45:16 +0900

    fix

--- firstapp/src/main/java/com/dw/employeeManagement/service/impl/EmployeeServiceImpl.java
+++ firstapp/src/main/java/com/dw/employeeManagement/service/impl/EmployeeServiceImpl.java
@@ -2,15 +2,19 @@ package com.dw.employeeManagement.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.dw.employeeManagement.model.Employee;
 import com.dw.employeeManagement.service.EmployeeService;
 
+@Service
 public class EmployeeServiceImpl implements EmployeeService{
 
 	// 직원정보를 새로 생성한다
 	@Override
     public Employee saveEmployee(Employee employee) {
-    	return null;
+		System.out.println(employee.getFirstName());
+    	return employee;
     }
 	// 전체 직원정보를 조회한다
 	@Override
