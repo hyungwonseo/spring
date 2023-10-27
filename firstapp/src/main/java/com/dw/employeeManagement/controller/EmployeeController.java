0a86e605d3a24e2dd4704e2b6415fdd78f6358d4
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2023-10-27 17:29:49 +0900

    fix

--- firstapp/src/main/java/com/dw/employeeManagement/controller/EmployeeController.java
+++ firstapp/src/main/java/com/dw/employeeManagement/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +53,24 @@ public class EmployeeController {
 		return new ResponseEntity<Employee>(
 				employeeService.getEmployeeById(id), HttpStatus.OK);
 	}
+	
+	// ID로 직원 한명의 정보를 업데이트한다
+	@PutMapping("{id}")
+	public ResponseEntity<Employee> updateEmployeeById(
+			@RequestBody Employee employee, @PathVariable long id) {
+		return new ResponseEntity<Employee>(
+				employeeService.updateEmployeeById(employee, id),
+				HttpStatus.OK);
+	}
+	
+	// ID로 직원 한명의 정보를 삭제한다
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable long id) {
+		employeeService.deleteEmployee(id);
+		return new ResponseEntity<String>("Employee " + id + " deleted successfully"
+				, HttpStatus.OK);
+	}
+	
 }
 
 
--- firstapp/src/main/java/com/dw/employeeManagement/service/impl/EmployeeServiceImpl.java
+++ firstapp/src/main/java/com/dw/employeeManagement/service/impl/EmployeeServiceImpl.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.dw.employeeManagement.exception.ResourceNotFoundException;
 import com.dw.employeeManagement.model.Employee;
 import com.dw.employeeManagement.repository.EmployeeRepository;
 import com.dw.employeeManagement.service.EmployeeService;
@@ -35,21 +35,45 @@ public class EmployeeServiceImpl implements EmployeeService{
 	// ID로 직원 한명의 정보를 조회한다
 	@Override
 	public Employee getEmployeeById(long id) {
-		Optional<Employee> employee = employeeRepository.findById(id);
-		if (employee.isPresent()) {
-			return employee.get();
-		}else {
-			return null;
-		} 
+//		Optional<Employee> employee = employeeRepository.findById(id);
+//		if (employee.isPresent()) {
+//			return employee.get();
+//		}else {
+//			return null;
+//		}
+		return employeeRepository.findById(id).orElseThrow(()->
+				new ResourceNotFoundException("Employee", "ID", id));
 	}
 	// ID로 직원 한명의 정보를 업데이트한다
 	@Override
 	public Employee updateEmployeeById(Employee employee, long id) {
-		return null;
+		Employee existingEmployee = employeeRepository.findById(id)
+				.orElseThrow(()->new ResourceNotFoundException("Employee", "ID", id));
+		existingEmployee.setFirstName(employee.getFirstName());
+		existingEmployee.setLastName(employee.getLastName());
+		existingEmployee.setEmail(employee.getEmail());
+		
+		employeeRepository.save(existingEmployee);
+		return existingEmployee;
 	}
 	// ID로 직원 한명의 정보를 삭제한다
 	@Override
 	public void deleteEmployee(long id) {
-		
+		employeeRepository.findById(id).orElseThrow(()->
+				new ResourceNotFoundException("Employee", "ID", id));
+		employeeRepository.deleteById(id);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
